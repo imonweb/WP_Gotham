82e3a9cadc9623821eb51020cfea13bdfb94e2bd
Imon <contact@imonweb.co.uk>
2023-11-08 02:09:19 +0000

    curvy plugin start

---
+++ plugins/curvy/build/blocks/curvy/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0b5932d48bc8f6d84e21');
--- plugins/curvy/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5f1d2c48dff7e86301e7');
