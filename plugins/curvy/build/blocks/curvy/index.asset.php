57d8cb0b762eb0b01a5dd284a8e7383c715f15eb
Imon <contact@imonweb.co.uk>
2023-11-27 23:40:16 +0000

    flip vertical and horizontal attributes

--- plugins/curvy/build/blocks/curvy/index.asset.php
+++ plugins/curvy/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'dcd4582fc1149adfd080');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f12f2bb8e080cc59bc24');
