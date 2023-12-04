f1d86badf6ee6f6ed849a3ab3d0fa5522b89aa3e
Imon <contact@imonweb.co.uk>
2023-12-04 23:43:14 +0000

    create block preview

--- plugins/curvy/build/blocks/curvy/index.asset.php
+++ plugins/curvy/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f50ea34a38728a186803');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6ae13838414c0d2026cd');
