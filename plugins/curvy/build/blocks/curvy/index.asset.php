4d66d425b939c1c1b065f8bc9040ade52a0b958c
Imon <contact@imonweb.co.uk>
2023-12-04 23:33:23 +0000

    create block variations

--- plugins/curvy/build/blocks/curvy/index.asset.php
+++ plugins/curvy/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3bbdee81cc51cf16c92e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f50ea34a38728a186803');
